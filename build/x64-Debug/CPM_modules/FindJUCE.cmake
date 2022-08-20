f6cdd61abecf54e22a5bfdf364b997abfa8044c5
Tudor Croitoru <ihorvalds@outlook.com>
2022-08-20 12:16:36 +0300

    Delete build/x64-Debug directory

--- build/x64-Debug/CPM_modules/FindJUCE.cmake
+++
@@ -1,3 +0,0 @@
-include("D:/Practice Apps/AudioPluginsDev/Trempoline/cmake/CPM.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;juce-framework/JUCE;GIT_TAG;master;EXCLUDE_FROM_ALL;YES")
-set(JUCE_FOUND TRUE)
